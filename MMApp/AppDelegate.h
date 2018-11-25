b7ca1b76b34e24098cd6f5829e0574fdd47b28da
橙晓侯 <1019890763@qq.com>
2018-11-25 12:16:43 +0800

    Initial Commit

---
+++ MMApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MMApp
+//
+//  Created by 橙晓侯 on 2018/11/25.
+//  Copyright © 2018 橙晓侯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MMApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MMApp
+//
+//  Created by 橙晓侯 on 2018/11/25.
+//  Copyright © 2018 橙晓侯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
